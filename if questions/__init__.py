a3b4756943039e13181c6f43a308c8d26b49d41f
Sidhant <sidhantdahiya86@gmail.com>
2024-07-21 13:41:32 +0530

    first commit

---
+++ if questions/__init__.py
@@ -0,0 +1,90 @@
+# s = "hey guys you all are osam"
+# print(len(s))
+# if len(s)%2==0:
+#     print("the length is even")
+# else:
+#     print("the length is odd")
+
+# a= 98
+# b= 67
+# if a>b:
+#     print("a is greater")
+# else:
+#     print("b is greater")
+
+# a = int(input("enter the number"))
+# if a%5==0:
+#     print("the number is divisible by 5")
+# else:
+#     print("the number is not divisible by 5")
+
+# a = int(input("enter the number"))
+# if (a%2==0):
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+# a = int((input("enter the score")))
+# if (a>=70):
+#     print("good luck")
+
+# a = int(input("enter the age"))
+# if (a>=18):
+#     print("you are eligible")
+
+# a = int(input("enter the value"))
+# if (a>1,a<5):
+#     print("python coding")
+
+# a = int(input("enter the number"))
+# if (a<=0):
+#     print("its negative guys")
+
+# a = int(input("enter the number"))
+# if (a>=0):
+#     print("the number is even")
+
+# a = input("enter the value")
+# print (type(a))
+
+# a = input("enter the string")
+# if a.isupper():
+#     print("string is upper")
+# else:
+#     print("string is lower")
+
+# a = int(input("enter the number"))
+# if (a%3==0):
+#     print("the number is divisbile by 3")
+# else:
+#     print("the number is not divisible by 3")
+
+# a = input("enter the number")
+# if (a%5==0):
+#     print("hello")
+# else:
+#     print("bye")
+
+# p = ["java","python","c","c++","ruby","golang"]
+# a = input("enter the string")
+# if a in p:
+#     print("the given language is present")
+# else:
+#     print("the language doesnt exists")
+
+# a = int(input("enter the number"))
+# if a>0:
+#     print("the number is positive")
+
+# a = input("enter the value")
+# if a==a[::-1]:
+#     print("it is palindrome")
+# else:
+#     print("the word is not palindrome")
+
+# v = ("aeiouAEIOU")
+# s ="Lahari is a good student"
+# if s[0] not in v:
+#     print("the letter is consonent ")
+
+
